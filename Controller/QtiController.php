570ca7d2d6f1cc0b879d3ff58033143a1518e42b
david <david@DavidB>
2015-05-27 10:40:04 +0200

    prepare to remove command "exec"

--- Controller/QtiController.php
+++ Controller/QtiController.php
@@ -113,6 +113,7 @@ class QtiController extends Controller {
         $zip->close();
         $fs = new FileSystem();
 
+        //if the xml is in subdirectory and not in the root
         foreach ($root as  $infoFichier){
             if (count($infoFichier) > 1) {
                 unset($infoFichier[count($infoFichier) - 1]);
@@ -120,6 +121,8 @@ class QtiController extends Controller {
                 //please use $fs->move() instead
                 //@see http://symfony.com/doc/current/components/filesystem/introduction.html
                 exec('mv '.$qtiRepos->getUserDir().$comma_separated.'/* '.$qtiRepos->getUserDir());
+                //$sf = new FileSystem();
+                //$sf->copyDir($qtiRepos->getUserDir().$comma_separated, $qtiRepos->getUserDir());die();
             }
         }
 
