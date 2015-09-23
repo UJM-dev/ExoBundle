a8dd7eb3e83eb48acf72e44f92a45425b3c8c665
ngodfraind <nicolas.godfraind@gmail.com>
2015-09-23 15:55:35 +0200

    Removing exec from qtiRepository (there is one left in QtiController)

--- Services/classes/QTI/qtiRepository.php
+++ Services/classes/QTI/qtiRepository.php
@@ -90,16 +90,8 @@ class qtiRepository {
      */
     public function removeDirectory()
     {
-         if(is_dir($this->userRootDir)) {
-            exec ('rm -rf '.$this->userRootDir.'*');
-            $fs = new FileSystem();
-            $iterator = new \DirectoryIterator($this->userRootDir);
-
-            foreach ($iterator as $el) {
-                if ($el->isDir()) $fs->rmDir($el->getRealPath(), true);
-                if ($el->isFile()) $fs->rm($el->getRealPath());
-            }
-        }
+        $fs = new FileSystem();
+        $fs->rmdir($this->userRootDir, true);
     }
 
     /**
